459dc6ba6d25bb8e306b106e05c7d3ebc9cd7cb4
Cesar Acual <chechaacual@gmail.com>
2018-11-01 18:58:00 -0600

    1.Primeros pasos

---
+++ database/seeds/BookTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+use App\Book;
+use Illuminate\Database\Seeder;
+
+class BookTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Book::class, 20)->create();
+    }
+}
