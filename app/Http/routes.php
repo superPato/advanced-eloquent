1f31741fcbcb2f50276db6bb5d727cb0c358f969
Cesar Acual <chechaacual@gmail.com>
2018-12-04 21:29:40 -0600

    Consulta de relaciones personalizadas con whereHas()

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,9 @@
 */
 
 Route::get('/', function () {
-	$categories = App\Category::has('books')->get();
+	$categories = App\Category::whereHas('books', function ($query) {
+		$query->where('status', 'public');
+	})->get();
 
 	return view('relationship', compact('categories'));
 });
